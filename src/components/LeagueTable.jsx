1335a662b887346f78e38e3980c2e931ecdea59a
Ryan Woods <38187920+ryan50500@users.noreply.github.com>
2026-03-27 20:32:20 +0000

    adding borders to table data

--- src/components/LeagueTable.jsx
+++ src/components/LeagueTable.jsx
@@ -1,10 +1,10 @@
+import './LeagueTable.css'
 import TableHeader from './TableHeader'
 import TeamList from './TeamList'
 
 export default function LeagueTable() {
   return (
     <div className="leagueTable__wrapper">
-      <h1 className="leagueTable__title">Premier League 2025/26</h1>
       <div className="leagueTable">
         <TableHeader />
         <TeamList />
