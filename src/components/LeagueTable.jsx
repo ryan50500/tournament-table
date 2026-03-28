dd14f07e1d711f72808a818cc0c5d42f38fff730
Ryan Woods <38187920+ryan50500@users.noreply.github.com>
2026-03-28 15:32:46 +0000

    removing wrapper as its not needed

--- src/components/LeagueTable.jsx
+++ src/components/LeagueTable.jsx
@@ -4,11 +4,9 @@ import TeamList from './TeamList'
 
 export default function LeagueTable() {
   return (
-    <div className="leagueTable__wrapper">
       <div className="leagueTable">
         <TableHeader />
         <TeamList />
       </div>
-    </div>
   )
 }
