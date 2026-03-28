869ddcfb82c7ff3329273f175a5893abbfea5193
Ryan Woods <38187920+ryan50500@users.noreply.github.com>
2026-03-28 16:00:12 +0000

    adding wrapper back for league table to better demonstrate sticky columns and header working

--- src/components/LeagueTable.jsx
+++ src/components/LeagueTable.jsx
@@ -4,9 +4,11 @@ import TeamList from './TeamList'
 
 export default function LeagueTable() {
   return (
+    <div className="leagueTable__wrapper">
       <div className="leagueTable">
         <TableHeader />
         <TeamList />
       </div>
+    </div>
   )
 }
